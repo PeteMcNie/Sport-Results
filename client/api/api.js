7967fc93fa56b25f9960d720d27d632a34fe6438
petemcnie <petemcnie0@gmail.com>
2020-09-21 18:07:05 +1200

    Installed superagent and getTeam post function written

--- client/api/api.js
+++ client/api/api.js
@@ -1,3 +1,21 @@
+// export function getTeam (team) {
+//   console.log('api: getteam hit', team)
+//   fetch('/home', { method: 'POST', body: team })
+//     .then(response => {
+//         console.log('api POST response', response)
+//         return response.body
+//     })
+// }
+
+import request from 'superagent'
+
 export function getTeam (team) {
   console.log('api: getteam hit', team)
+  return request
+    .post('/home')
+    .send(team)
+    .then(response => {
+      console.log('api POST response', response.body)
+      return response.body
+    })
 }
