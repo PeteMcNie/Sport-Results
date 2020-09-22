93111018e0023f27528a87606f4707666f9aec75
petemcnie <petemcnie0@gmail.com>
2020-09-23 10:33:41 +1200

    Changed to get request and included team in params

--- client/api/api.js
+++ client/api/api.js
@@ -9,11 +9,12 @@
 
 import request from 'superagent'
 
+const footballUrl = '/home/v1/fb'
+
 export function getTeam (team) {
   console.log('api: getteam hit', team)
   return request
-    .post('/home')
-    .send(team)
+    .get(`${footballUrl}/${team}`)
     .then(response => {
       console.log('api POST response', response.body)
       return response.body
