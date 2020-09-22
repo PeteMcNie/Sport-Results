6225779e2eafd6625f3c2c2a854869d918afdb38
petemcnie <petemcnie0@gmail.com>
2020-09-23 10:34:01 +1200

    Set up sever

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ const express = require('express')
 
 const server = express()
 
+const fbroutes = require('./routes/footballroutes')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
+server.use('/home/v1/fb', fbroutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
