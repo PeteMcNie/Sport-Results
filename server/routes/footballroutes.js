85567962b7cd18f4d23c1213146fef378ee9d589
petemcnie <petemcnie0@gmail.com>
2020-09-23 11:10:58 +1200

    Football api data retrieved in football routes

--- server/routes/footballroutes.js
+++ server/routes/footballroutes.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const request = require('superagent')
 
 const router = express.Router()
 
 module.exports = router
 
 router.get('/:team', (req, res) => {
-    const team = req.params
+    let team = 66
     console.log('router', team)
+ 
+
+    return request
+        .get(`https://api.football-data.org/v2/teams/${team}/matches/`)
+        .set('X-Auth-Token', '9a0717303f9a43ef8574f6f0a14dee51')
+        .then(res => {
+            console.log('fb api res', res.body)
+        })
+
+
+    //'https://api.football-data.org/v2/competitions/PL/matches' //ALL MATCHES FOR THAT COMPETION
+    // '/v2/competitions/{id = PL}/standings'  ALL STANDINGS FOR THAT COMPETION
+    // '/v2/teams/{id}/matches/'  ALL MATCHES FOR A TEAM
+
+
 })
