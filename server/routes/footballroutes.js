6b31da03e7260c897f3627dd38c03cc2d043f33c
petemcnie <petemcnie0@gmail.com>
2020-09-23 13:32:24 +1200

    Refactored get request to football routes

--- server/routes/footballroutes.js
+++ server/routes/footballroutes.js
@@ -1,24 +1,11 @@
 const express = require('express')
 const request = require('superagent')
 
+const trimData = require('../functions/trimData')
+
 const router = express.Router()
 
 module.exports = router
-
-function trimInfo (teamInfo) {
-  const trimmedInfo = teamInfo.body.matches.map(function (el) {
-    return {
-      awayTeam: el.awayTeam,
-      hometeam: el.homeTeam,
-      score: el.score,
-      matchday: el.matchday,
-      data: el.date,
-      competition: el.competition,
-      status: el.status
-    }
-  })
-  return trimmedInfo
-}
 
 router.get('/:team', (req, res) => {
   const team = req.params.team
@@ -27,20 +14,16 @@ router.get('/:team', (req, res) => {
     .get(`https://api.football-data.org/v2/teams/${team}/matches/`)
     .set('X-Auth-Token', '9a0717303f9a43ef8574f6f0a14dee51')
     .then(teamInfo => {
-      const trimmedInfo = trimInfo(teamInfo)
-      return trimmedInfo
-    })
-    .then(trimmedInfo => {
+      const trimmedInfo = trimData(teamInfo)
       res.json(trimmedInfo)
     })
     .catch(err => {
-      console.error('ERROR IN FOOTBALL GET ROUTE', err)
-      res.status(500).send(err.message)
+      res.status(500).send('ERROR IN FOOTBALL GET ROUTE', err.message)
     })
 })
 
 // FURTHER API CALLS COULD BE MADE USING THE BELOW:
 
-// 'https://api.football-data.org/v2/competitions/PL/matches' //ALL MATCHES FOR THAT COMPETION 
+// 'https://api.football-data.org/v2/competitions/PL/matches' //ALL MATCHES FOR THAT COMPETION
 // '/v2/competitions/{id = PL}/standings'  ALL STANDINGS FOR THAT COMPETION
 // '/v2/teams/{id}/matches/'  ALL MATCHES FOR A TEAM
