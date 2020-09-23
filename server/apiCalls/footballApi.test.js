b32c905b04445ba252c1c7354b7db59456360481
petemcnie <petemcnie0@gmail.com>
2020-09-23 16:36:26 +1200

    footballApi.test file set up

---
+++ server/apiCalls/footballApi.test.js
@@ -0,0 +1,39 @@
+const getTeamInfo = require('./footballApi')
+// const server = require('../server')
+
+jest.mock('./footballApi', () => {
+  return {
+    getTeamInfo: jest.fn()
+  }
+})
+
+test('returns football api data', () => {
+  const teamRequested = 66
+
+  getTeamInfo.mockImplementation(() => Promise.resolve(
+    {
+      date: 'today',
+      status: 'Played'
+    }
+  ))
+
+  return getTeamInfo(teamRequested)
+    .then(teamInfo => {
+      expect(teamInfo.date).toBe('Sandwiches')
+    })
+})
+
+// describe('GET REQUEST', () => {
+//   it('should get football team information', async done => {
+//     const res = await request(server)
+//       .get('https://api.football-data.org/v2/teams/66/matches/')
+//       .set('X-Auth-Token', `${key.key}`)
+//     // .expect('Content-Type', /json/)
+//     // .expect(200)
+//     // .then(res => {
+//     //   expect(res.body.date).toEqual('sandwich')
+
+//     expect(res.status).toEqual(200)
+//     done()
+//   })
+// })
