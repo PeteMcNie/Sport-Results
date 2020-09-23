0ad10e28bd6c0d376d3ac40a8f39116ac1025f69
petemcnie <petemcnie0@gmail.com>
2020-09-24 11:23:42 +1200

    Altered footballApi file so it runs with a cronjob

--- server/apiCalls/footballApi.js
+++ server/apiCalls/footballApi.js
@@ -1,16 +1,26 @@
 const request = require('superagent')
 
 const key = require('../tools/footballApiKey')
-const trimData = require('../functions/trimData')
+// const trimData = require('../functions/trimData')
 
 function getTeamInfo (team) {
   return request
     .get(`https://api.football-data.org/v2/teams/${team}/matches/`)
     .set('X-Auth-Token', `${key.key}`)
     .then(teamInfo => {
-      const trimmedInfo = trimData(teamInfo)
-      return trimmedInfo
+      const data = teamInfo.body.matches
+      // const trimmedInfo = trimData(teamInfo)
+      console.log('CRONJOB CALL', teamInfo.body.matches)
+      return data
+    })
+    .catch(err => {
+      throw err.status(500).send('ERROR COLLECTING DATA FROM FOOTBALL API', err.message)
     })
 }
 
+getTeamInfo(66) // Manchester United id
+getTeamInfo(397) // Brighton id
+
 module.exports = getTeamInfo
+
+
--- server/apiCalls/footballApi.test.js
+++ server/apiCalls/footballApi.test.js
@@ -22,18 +22,3 @@ xtest('returns football api data', () => {
       expect(teamInfo.date).toBe('Sandwiches')
     })
 })
-
-// describe('GET REQUEST', () => {
-//   it('should get football team information', async done => {
-//     const res = await request(server)
-//       .get('https://api.football-data.org/v2/teams/66/matches/')
-//       .set('X-Auth-Token', `${key.key}`)
-//     // .expect('Content-Type', /json/)
-//     // .expect(200)
-//     // .then(res => {
-//     //   expect(res.body.date).toEqual('sandwich')
-
-//     expect(res.status).toEqual(200)
-//     done()
-//   })
-// })
