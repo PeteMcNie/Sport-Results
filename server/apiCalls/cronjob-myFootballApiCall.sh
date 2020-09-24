11b4baa2e4bbbfaa2c993b687db3a58637f381be
petemcnie <petemcnie0@gmail.com>
2020-09-24 12:59:21 +1200

    Changed cronjob file path

--- server/apiCalls/cronjob-myFootballApiCall.sh
+++ server/apiCalls/cronjob-myFootballApiCall.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-node ./footballApi.js
+node ./server/apiCalls/footballApi.js
 
--- server/apiCalls/footballApi.js
+++ server/apiCalls/footballApi.js
@@ -8,10 +8,10 @@ function getTeamInfo (team) {
     .get(`https://api.football-data.org/v2/teams/${team}/matches/`)
     .set('X-Auth-Token', `${key.key}`)
     .then(teamInfo => {
-      const data = teamInfo.body.matches
+      // const data = teamInfo.body.matches
       // const trimmedInfo = trimData(teamInfo)
       console.log('CRONJOB CALL', teamInfo.body.matches)
-      return data
+      return teamInfo.body.matches
     })
     .catch(err => {
       throw err.status(500).send('ERROR COLLECTING DATA FROM FOOTBALL API', err.message)
