941509dc1f527111ca166dab35c5fb8ac27e453e
petemcnie <petemcnie0@gmail.com>
2020-10-05 11:38:13 +1300

    Displaying football data

---
+++ client/components/TeamMatchData.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+function TeamMatchData (props) {
+  const { teamMatchData } = props
+  console.log('DATA IN COMP ', teamMatchData)
+  if (!teamMatchData) return null
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>Home Team</th>
+          <th>Away Team</th>
+          <th>Winner</th>
+        </thead>
+        <tbody>
+          {teamMatchData.map(el =>
+            <tr key={el.id}>
+              <td>{el.homeTeam.name}</td>
+              <td>{el.awayTeam.name}</td>
+              <td>{el.score.winner}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+
+  )
+}
+
+function mapStateToProps (state) {
+  return {
+    teamMatchData: state.teamMatchData
+  }
+}
+export default connect(mapStateToProps)(TeamMatchData)
