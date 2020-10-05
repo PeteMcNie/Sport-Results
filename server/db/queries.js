2251ded52fc120eedc9625ea0b9347df52a7cb5f
petemcnie <petemcnie0@gmail.com>
2020-10-05 15:56:16 +1300

    Connected to postgres db

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,10 +1,37 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'matches',
-  password: 'password',
-  port: 3000
+// const { Pool } = require('pg')
+// const pool = new Pool({
+//     user: 'me',
+//     host: 'localhost',
+//     database: 'matches',
+//     password: 'password',
+//     port: 5432
+// })
+
+function getMatches (req, res) {
+//  pool.query('SELECT * FROM matches ORDER BY id ASC', (err, res) => {
+//     if (err) {
+//       throw err
+//     }
+//     res.send(res.rows)
+//   })
+}
+
+module.exports = {
+  getMatches
+}
+
+const { Client } = require('pg')
+const client = new Client({
+  user: 'postgres', // Also found in pgAdmin, master folder before db folder I named
+  host: 'localhost', // Server name or IP address
+  database: 'petemcnie', // Name of my db, see pgAdmin
+  password: 'password', // Password I set
+  port: 5433
 })
 
-module.exports = pool
+client.connect()
+  .then(() => console.log('Connected Successfully'))
+  .then(() => client.query('SELECT * FROM matches'))
+  .then((response) => console.log(response))
+  .catch(err => console.log(err))
+  .finally(() => client.end())
