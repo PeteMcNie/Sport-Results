ab233429a38b318059ac4807c1b9c3cd484fa8e4
Pete McNie <petemcnie0@gmail.com>
2020-10-14 13:08:33 +1300

    Added and linked new Links component to the main page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import Title from './Title'
+import Links from './Links'
 import TeamRequest from './TeamRequest'
 import TeamMatchData from './TeamMatchData'
 
@@ -9,8 +10,9 @@ const App = () => {
   return (
     <>
       <Route path='/' component={Title} />
-      <Route path='/' component={TeamRequest} />
-      <Route path='/' component={TeamMatchData} />
+      <Route path='/' component={Links} />
+      <Route path='/team' component={TeamRequest} />
+      <Route path='/team' component={TeamMatchData} />
     </>
   )
 }
