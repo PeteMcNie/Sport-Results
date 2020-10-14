cc96eb29de6d5169ad276daa7bc361ad209a6786
Pete McNie <petemcnie0@gmail.com>
2020-10-15 10:46:37 +1300

    Add sports folder and main sports component

---
+++ client/components/Sports/TeamMatchData.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+function TeamMatchData (props) {
+  const { teamMatchData } = props
+  console.log('DATA IN COMP ', teamMatchData)
+  if (!teamMatchData) return null
+
+  return (
+    <div>
+      {teamMatchData.map(el =>
+      <table>
+        <thead>
+          <tr>
+            <th>Matchday</th>
+            <th>Home Team</th>
+            <th>Away Team</th>
+            <th>Winner</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr key={el.id}>
+              <td>{el.matchday}</td>
+              <td>{el.homeTeam.name}</td>
+              <td>{el.awayTeam.name}</td>
+              <td>{el.score.winner}</td>
+            </tr>
+        </tbody>
+      </table>
+      )}
+    </div>
+
+  )
+}
+
+function mapStateToProps (state) {
+  return {
+    teamMatchData: state.teamMatchData
+  }
+}
+export default connect(mapStateToProps)(TeamMatchData)
