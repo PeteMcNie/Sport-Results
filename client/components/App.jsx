4b13d9a40d8761ff74571637a9f7ea1d65957402
Pete McNie <petemcnie0@gmail.com>
2020-10-15 10:27:58 +1300

    Add routes for new Sports and SpaceX components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,18 +3,22 @@ import { Route } from 'react-router-dom'
 
 import Title from './Title'
 import Links from './Links'
-import TeamRequest from './TeamRequest'
-import TeamMatchData from './TeamMatchData'
-import SpaceXRequest from './SpaceXRequest'
+import Sports from './Sports'
+import SpaceX from './SpaceX'
+// import TeamRequest from './TeamRequest'
+// import TeamMatchData from './TeamMatchData'
+// import SpaceXRequest from './SpaceXRequest'
 
 const App = () => {
   return (
     <>
       <Route exact path='/' component={Links} />
       <Route path='/' component={Title} />
-      <Route path='/sportresults' component={TeamRequest} />
+      <Route path='/sportresults' component={Sports} />
+      <Route path='spaceX' component={SpaceX} />
+      {/* <Route path='/sportresults' component={TeamRequest} />
       <Route path='/sportresults' component={TeamMatchData} />
-      <Route path='/spaceX' component={SpaceXRequest} />
+      <Route path='/spaceX' component={SpaceXRequest} /> */}
     </>
   )
 }
