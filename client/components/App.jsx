623f82a3c1ad3bcb653d90fd48c1bfd0392cc70b
Pete McNie <petemcnie0@gmail.com>
2020-10-15 10:47:28 +1300

    Update App to reflect new folder structure, debugged broken spaceX route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import Title from './Title'
-import Links from './Links'
+import Links from './Links/Links'
 import Sports from './Sports'
 import SpaceX from './SpaceX'
-// import TeamRequest from './TeamRequest'
-// import TeamMatchData from './TeamMatchData'
-// import SpaceXRequest from './SpaceXRequest'
 
 const App = () => {
   return (
@@ -15,10 +12,7 @@ const App = () => {
       <Route exact path='/' component={Links} />
       <Route path='/' component={Title} />
       <Route path='/sportresults' component={Sports} />
-      <Route path='spaceX' component={SpaceX} />
-      {/* <Route path='/sportresults' component={TeamRequest} />
-      <Route path='/sportresults' component={TeamMatchData} />
-      <Route path='/spaceX' component={SpaceXRequest} /> */}
+      <Route path='/spaceX' component={SpaceX} />
     </>
   )
 }
--- client/components/SpaceXRequest.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SpaceXRequest () {
-    return (
-        <h1>Hello SpaceX</h1>
-    )
-}
-
-export default SpaceXRequest
