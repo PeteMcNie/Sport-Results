63819a33b0e0f711df85b47b6b04ee3ad3815767
Pete McNie <petemcnie0@gmail.com>
2020-10-15 16:22:38 +1300

    Add form and state to component

--- client/components/SpaceX/SpaceXRequest.jsx
+++ client/components/SpaceX/SpaceXRequest.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
 
-function SpaceXRequest () {
+function SpaceXRequest (props) {
+    const [state, setState] = useState('')
+
+    function handleChange (evt) {
+        const { value } = evt.target
+        setState(value)
+    }
+
+    function submitHandler () {
+        console.log('spacexrequest ', state)
+        if (state) {
+            props.dispatch(getTheSpaceData(state))
+        }
+    }
+
     return (
-        <h1>Hello SpaceX</h1>
+        <div>
+            <form onSubmit={() => submitHandler}>
+                <input onChange={handleChange} value={state}></input>
+                <button>Blast off!</button>
+            </form>
+        </div>
     )
 }
 
-export default SpaceXRequest
+export default connect()(SpaceXRequest)
