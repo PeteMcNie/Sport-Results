ddb8ca8a5798bd2361281e02a7cfb88359e9b1e7
petemcnie <petemcnie0@gmail.com>
2020-10-20 10:41:52 +1300

    New links created

--- client/components/Links/HomeLink.jsx
+++ client/components/Links/HomeLink.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function HomeLink () {
-    return (
-        <div className='homecontainer'>
-            <Link to="/" className='homelink'>Home</Link>
-        </div>
-    )
+  return (
+    <div className='homecontainer'>
+      <Link to="/" className='homelink'>Home</Link>
+    </div>
+  )
 }
 
 export default HomeLink
