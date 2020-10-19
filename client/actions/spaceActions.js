6f3b45d0a3c184d1852f9a20038d161e900bfea6
petemcnie <petemcnie0@gmail.com>
2020-10-20 10:58:33 +1300

    Add spacex thunk action

--- client/actions/spaceActions.js
+++ client/actions/spaceActions.js
@@ -2,3 +2,18 @@ export const GET_SPACEX_DATA = 'GET_SPACEX_DATA'
 export const GET_SPACEX_DATA_SUCCESS = 'GET_SPACEX_DATA_SUCCESS'
 export const GET_SPACEX_DATA_FAILURE = 'GET_SPACEX_DATA_FAILURE'
 
+export function getSpaceData (data) {
+  return (dispatch) => {
+    dispatch(gettingSpaceXData())
+    return getSpaceXData(data)
+      .then(returnData => {
+        console.log(returnData)
+      })
+  }
+}
+
+export function gettingSpaceXData () {
+  return {
+    type: GET_SPACEX_DATA
+  }
+}
