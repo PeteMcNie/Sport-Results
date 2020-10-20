8f2a75d88214d4516a43d39c1085964612f62385
petemcnie <petemcnie0@gmail.com>
2020-10-21 09:25:44 +1300

    Delete console statment

--- client/api/spacexApi.js
+++ client/api/spacexApi.js
@@ -3,8 +3,8 @@ import request from 'superagent'
 const spaceXUrl = '/home/v1/spacex'
 
 export function getSpaceXData (data) {
-  console.log('SPACEX api: ', data)
   const stringData = data.SpaceX
+  //   console.log('SPACEX api string: ', stringData)
   return request
     .get(`${spaceXUrl}/${stringData}`)
     .then(response => {
