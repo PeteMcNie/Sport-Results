fb5ef7cd1d9e36e5dd51c14c88837b84491d9e2f
petemcnie <petemcnie0@gmail.com>
2020-10-21 09:15:23 +1300

    Get string from object in form

--- client/api/spacexApi.js
+++ client/api/spacexApi.js
@@ -4,9 +4,10 @@ const spaceXUrl = '/home/v1/spacex'
 
 export function getSpaceXData (data) {
   console.log('SPACEX api: ', data)
+  const stringData = data.SpaceX
   return request
-    .get(`${spaceXUrl}/${data}`)
+    .get(`${spaceXUrl}/${stringData}`)
     .then(response => {
-      console.log('spacex GET request RESPONSE: ', response)
+      console.log('spacex GET request RESPONSE: ', response.body)
     })
 }
