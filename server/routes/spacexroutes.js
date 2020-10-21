7e52a5318c2c085170a1feccbafb7a43568b351f
petemcnie <petemcnie0@gmail.com>
2020-10-22 12:04:55 +1300

    Add function call to route

--- server/routes/spacexroutes.js
+++ server/routes/spacexroutes.js
@@ -1,12 +1,15 @@
 const express = require('express')
-
 const router = express.Router()
-
 module.exports = router
+
+const spacexapi = require('../apiCalls/spacexApi')
 
 router.get('/', (req, res) => {
   console.log('ONLOAD SPACEX ROUTE HIT')
-  
+  spacexapi.getspacexdata()
+    .then(data => {
+      console.log('spacexdata in route', data)
+    })
 })
 
 router.get('/:data', (req, res) => {
