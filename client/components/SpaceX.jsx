27a96b9830823d65a0a38b9d01a72db61f7eae38
petemcnie <petemcnie0@gmail.com>
2020-10-21 15:19:38 +1300

    Add useEffect and useDispatch to call action when component mounts

--- client/components/SpaceX.jsx
+++ client/components/SpaceX.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 import SpaceXRequest from './SpaceX/SpaceXRequest'
 import HomeLink from './Links/HomeLink'
 
+import { getUpdatedSpaceXData } from '../actions/spaceActions'
+
 function SpaceX () {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getUpdatedSpaceXData())
+  })
+
   return (
     <>
       <SpaceXRequest />
