63486774853d78d43768b21056317950c40148f6
petemcnie <petemcnie0@gmail.com>
2020-10-21 15:21:20 +1300

    Add onloadGetSXData function

--- client/api/spacexApi.js
+++ client/api/spacexApi.js
@@ -2,6 +2,18 @@ import request from 'superagent'
 
 const spaceXUrl = '/home/v1/spacex'
 
+export function onloadGetSXData () {
+  // console.log('SPACEX API ONLOAD CALLED')
+  return request
+    .get(`${spaceXUrl}`)
+    .then(response => {
+      console.log('spacex ONLOAD request RESPONSE: ', response.body)
+    })
+    .catch(err => {
+      console.err('Error in spacexApi: onloadGetSXData', err)
+    })
+}
+
 export function getSpaceXData (data) {
   const stringData = data.SpaceX
   //   console.log('SPACEX api string: ', stringData)
@@ -9,5 +21,8 @@ export function getSpaceXData (data) {
     .get(`${spaceXUrl}/${stringData}`)
     .then(response => {
       console.log('spacex GET request RESPONSE: ', response.body)
+    })
+    .catch(err => {
+      console.err('Error in spacexApi: getSpaceXData', err)
     })
 }
