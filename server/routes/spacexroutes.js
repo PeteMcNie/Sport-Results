c5f536fc0e6fa0643ff38958089beb21de3432bd
petemcnie <petemcnie0@gmail.com>
2020-10-21 15:27:41 +1300

    Add route for onload function to hit

--- server/routes/spacexroutes.js
+++ server/routes/spacexroutes.js
@@ -4,6 +4,11 @@ const router = express.Router()
 
 module.exports = router
 
+router.get('/', (req, res) => {
+  console.log('ONLOAD SPACEX ROUTE HIT')
+  
+})
+
 router.get('/:data', (req, res) => {
   const data = req.params.data
   console.log('Spacexroutes data: ', data)
