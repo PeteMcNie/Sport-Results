2d1d2e12366eb5cc2496cf3be60ba00dec55866b
petemcnie <petemcnie0@gmail.com>
2020-10-21 15:20:44 +1300

    Add getUpdatedSpaceXData function

--- client/actions/spaceActions.js
+++ client/actions/spaceActions.js
@@ -1,4 +1,27 @@
-import { getSpaceXData } from '../api/spacexApi'
+import { getSpaceXData, onloadGetSXData } from '../api/spacexApi'
+
+export const ON_LOAD_GET_SPACEX_DATA = 'ON_LOAD_GET_SPACEX_DATA'
+export const ON_LOAD_GET_SPACEX_DATA_SUCCESS = 'ON_LOAD_GET_SPACEX_DATA_SUCCESS'
+export const ON_LOAD_GET_SPACEX_DATA_FAILURE = 'ON_LOAD_GET_SPACEX_DATA_FAILURE'
+
+export function getUpdatedSpaceXData () {
+  // console.log('ONLOAD SPACEX ACTION CALLED')
+  return (dispatch) => {
+    dispatch(onloadGettingSXData)
+    return onloadGetSXData()
+      .then(returnData => {
+        console.log('onload spacex action returning data: ', returnData)
+      })
+  }
+}
+
+export function onloadGettingSXData () {
+  return {
+    type: ON_LOAD_GET_SPACEX_DATA
+  }
+}
+
+
 
 export const GET_SPACEX_DATA = 'GET_SPACEX_DATA'
 export const GET_SPACEX_DATA_SUCCESS = 'GET_SPACEX_DATA_SUCCESS'
