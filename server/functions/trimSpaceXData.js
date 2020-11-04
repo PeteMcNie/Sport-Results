071710b2b7e81ccba0a95b6f988f75450714cabc
petemcnie <petemcnie0@gmail.com>
2020-11-05 12:46:02 +1300

    Add if statement to return only the flightnumbers above 100

--- server/functions/trimSpaceXData.js
+++ server/functions/trimSpaceXData.js
@@ -1,19 +1,22 @@
 function trimSpaceXData (data) {
-  const trimmedData = data.map(el => {
+  const latestResults = []
+  data.map(el => {
     // console.log('Inside map function ', el)
-    return {
-      flightNumber: el.flight_number,
-      dateUtc: el.date_utc,
-      name: el.name,
-      success: el.success,
-      details: el.details,
-      links: {
-        wikipedia: el.links.wikipedia,
-        article: el.links.article
-      }
+    if (el.flight_number >= 100) {
+      latestResults.push({
+        flightNumber: el.flight_number,
+        dateUtc: el.date_utc,
+        name: el.name,
+        success: el.success,
+        details: el.details,
+        links: {
+          wikipedia: el.links.wikipedia,
+          article: el.links.article
+        }
+      })
     }
   })
-  return trimmedData
+  return latestResults
 }
 
 module.exports = trimSpaceXData
