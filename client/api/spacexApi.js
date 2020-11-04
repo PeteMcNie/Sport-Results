b31e0fdfd2af0325b1226520ce8c4fd002f931b6
petemcnie <petemcnie0@gmail.com>
2020-11-05 12:54:24 +1300

    Returning response from server-side into actions

--- client/api/spacexApi.js
+++ client/api/spacexApi.js
@@ -3,11 +3,10 @@ import request from 'superagent'
 const spaceXUrl = '/home/v1/spacex'
 
 export function onloadGetSXData () {
-  // console.log('SPACEX API ONLOAD CALLED')
   return request
     .get(`${spaceXUrl}`)
     .then(response => {
-      console.log('spacex ONLOAD request RESPONSE: ', response.body)
+      return response.body
     })
     .catch(err => {
       console.error('Error in spacexApi: onloadGetSXData', err)
