6737e4ef3c50f7ed033ab9fca48514930fc9ca5c
petemcnie <petemcnie0@gmail.com>
2020-11-05 12:05:47 +1300

    Trimmed spacex data down to wanted data

--- server/functions/trimSpaceXData.js
+++ server/functions/trimSpaceXData.js
@@ -2,7 +2,15 @@ function trimSpaceXData (data) {
   const trimmedData = data.map(el => {
     // console.log('Inside map function ', el)
     return {
-      flightNumber: el.flight_number
+      flightNumber: el.flight_number,
+      dateUtc: el.date_utc,
+      name: el.name,
+      success: el.success,
+      details: el.details,
+      links: {
+        wikipedia: el.links.wikipedia,
+        article: el.links.article
+      }
     }
   })
   // console.log('TRIMMED DATA IN TRIMSPACEXDATA ', trimmedData)
