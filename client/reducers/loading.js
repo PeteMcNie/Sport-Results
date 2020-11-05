dd7912f30712ffcfc0dc6be37b37772c0691e13e
petemcnie <petemcnie0@gmail.com>
2020-11-05 14:48:23 +1300

    Add input actions to loading reducer

--- client/reducers/loading.js
+++ client/reducers/loading.js
@@ -1,16 +1,22 @@
 import {
   ON_LOAD_GET_SPACEX_DATA,
   ON_LOAD_GET_SPACEX_DATA_SUCCESS,
-  ON_LOAD_GET_SPACEX_DATA_FAILURE
+  ON_LOAD_GET_SPACEX_DATA_FAILURE,
+  GET_SPACEX_DATA,
+  GET_SPACEX_DATA_SUCCESS,
+  GET_SPACEX_DATA_FAILURE
 } from '../actions/spaceActions'
 
 function loading (state = false, action) {
   switch (action.type) {
     case ON_LOAD_GET_SPACEX_DATA:
+    case GET_SPACEX_DATA:
       return true
 
     case ON_LOAD_GET_SPACEX_DATA_SUCCESS:
     case ON_LOAD_GET_SPACEX_DATA_FAILURE:
+    case GET_SPACEX_DATA_SUCCESS:
+    case GET_SPACEX_DATA_FAILURE:
       return false
 
     default:
