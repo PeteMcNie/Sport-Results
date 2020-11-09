190ef450429a4d1b88acb9f24fd5ea78e20847ab
petemcnie <petemcnie0@gmail.com>
2020-11-09 14:55:46 +1300

    Add spaceXdata component

---
+++ client/components/SpaceX/SpaceXData.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function SpaceXData () {
+  const sxd = useSelector(state => state.onloadSpaceXData)
+  console.log('SpaceXData', sxd)
+
+  sxd.map(el => {
+    if (el.success === true) {
+      el.success = 'True'
+    } else {
+      el.success = 'False'
+    }
+    return el.success
+  })
+
+  return (
+    <>
+      <table className='table'>
+        {sxd.map(el =>
+          <tbody key={el.flightNumber} className='tableblock'>
+            <tr className='headerrow'>
+              <td className='tableheader'>Flight No: {el.flightNumber}</td>
+              <td className='tableheader'>Name: {el.name}</td>
+              <td className='tableheader'>Date: {el.dateUtc}</td>
+            </tr>
+            <tr>
+              <td colSpan='3'className='tableinfo'>Details: {el.details}</td>
+            </tr>
+            <tr>
+              <td colSpan='3' className='tableinfo'>Mission Success: {el.success}</td>
+            </tr>
+            <tr>
+              <td colSpan='2' className='tableinfo'>More information: <a href="{el.links.wikipedia}">{el.links.wikipedia}</a></td>
+              <td className='tableinfo'><a href="{el.links.article}">{el.links.article}</a></td>
+            </tr>
+          </tbody>
+        )}
+
+      </table>
+    </>
+  )
+}
+
+export default SpaceXData
