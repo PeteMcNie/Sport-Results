1453f559559edf49d302ecd1e327fd369452dd0a
petemcnie <petemcnie0@gmail.com>
2020-11-12 09:20:44 +1300

    Once spacex data returns, call function to select rockets

--- server/routes/spacexroutes.js
+++ server/routes/spacexroutes.js
@@ -3,11 +3,17 @@ const router = express.Router()
 module.exports = router
 
 const spacexapi = require('../apiCalls/spacexApi')
+const selectRocketsUsedInLaunches = require('../functions/selectRockets')
 
 router.get('/', (req, res) => {
   spacexapi.getspacexdata()
+    .then(fullSpaceXData => {
+      const rockets = selectRocketsUsedInLaunches(fullSpaceXData)
+      const launches = fullSpaceXData[0]
+      const spacexData = { launches, rockets }
+      return spacexData
+    })
     .then(data => {
-      console.log('CHECK TO SEE IF DATA IS CORRECTLY BEING SENT TO: spacexroutes. THIS IS FINAL CHECK OF WHAT IS BEING SENT TO THE CLIENT SIDE ', data)
       res.status(200).send(data)
     })
     .catch(err => {
