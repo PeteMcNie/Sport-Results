3f0e44275fd3ca3cf66281b3c57421ec63a92460
petemcnie <petemcnie0@gmail.com>
2020-11-12 09:19:30 +1300

    Return promise all and refactor

--- server/apiCalls/spacexApi.js
+++ server/apiCalls/spacexApi.js
@@ -2,27 +2,14 @@ const request = require('superagent')
 
 const trimSpaceXData = require('../functions/trimSpaceXData')
 const trimRocketData = require('../functions/trimRocketData')
-const selectRocketsUsedInLaunches = require('../functions/selectRockets')
 
 const spacexLaunchUrl = 'https://api.spacexdata.com/v4/launches/past'
 const spacexRocketUrl = 'https://api.spacexdata.com/v4/rockets'
 
 async function getspacexdata () {
-  const spacexData = []
   const launches = await getAllPastLaunches()
   const allRockets = await getAllRockets()
-
-  Promise.all([launches, allRockets])
-    .then(data => {
-      const rockets = selectRocketsUsedInLaunches(data)
-      const spacexData = { launches, rockets }
-      console.log('getspacexdata LAUNCHES & ROCKETS ', spacexData)
-      return spacexData
-    })
-    .catch(err => {
-      throw err.status(500).send('ERROR IN PROMISE ALL getspacexdata function', err.message)
-    })
-  console.log(spacexData)
+  return Promise.all([launches, allRockets])
 }
 
 function getAllPastLaunches () {
@@ -52,8 +39,3 @@ function getAllRockets () {
 module.exports = {
   getspacexdata
 }
-
-// const allSpaceXRocketIds = data.body.map(el => el.id)
-// console.log('ALL SPACEX ROCKETS ', allSpaceXRocketIds)
-// const rockets = trimmedData.map(el => el.rocket)
-// console.log('ROCKETS ', rockets)
