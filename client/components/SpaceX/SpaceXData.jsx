6082e4fcc837376084e3249529ded1ddd559d931
petemcnie <petemcnie0@gmail.com>
2020-11-12 17:26:25 +1300

    Images displaying

--- client/components/SpaceX/SpaceXData.jsx
+++ client/components/SpaceX/SpaceXData.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 function SpaceXData () {
-  const sxd = useSelector(state => state.onloadSpaceXData.launches)
-  console.log('SpaceXData', sxd)
+  const launches = useSelector(state => state.onloadSpaceXData.launches)
+  console.log('SpaceXData', launches)
 
-  if (!sxd) {
+  const rockets = useSelector(state => state.onloadSpaceXData.rockets)
+
+  if (!launches && !rockets) {
     return null
   } else {
-    sxd.map(el => {
+    launches.map(el => {
       if (el.success === true) {
         el.success = 'True'
       } else {
@@ -20,7 +22,7 @@ function SpaceXData () {
     return (
       <>
         <table className='table'>
-          {sxd.map(el =>
+          {launches.map(el =>
             <tbody key={el.flightNumber} className='tableblock'>
               <tr className='headerrow'>
                 <td className='tableheader'>Flight No: {el.flightNumber}</td>
@@ -34,12 +36,19 @@ function SpaceXData () {
                 <td colSpan='3' className='tableinfo'>Mission Success: {el.success}</td>
               </tr>
               <tr>
-                <td colSpan='2' className='tableinfo'>More information: <a href="{el.links.wikipedia}">{el.links.wikipedia}</a></td>
-                <td className='tableinfo'><a href="{el.links.article}">{el.links.article}</a></td>
+                <td colSpan='2' className='tableinfo'>More information: <a href={el.links.wikipedia}>{el.links.wikipedia}</a></td>
+                <td className='tableinfo'><a href={el.links.article}>{el.links.article}</a></td>
               </tr>
             </tbody>
           )}
         </table>
+        <div>
+          {rockets[0].flickrImages.map(el =>
+            <div key={el}>
+              <img src={el} />
+            </div>
+          )}
+        </div>
       </>
     )
   }
